13903b49b38ab1b1e02e6f05568f13f2dd51ea54
Raavee Singh <gdg1465@gmail.com>
2026-08-02 20:03:38 +0530

    lec3

---
+++ lec3/sum-till-n-while.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(){
+    int n;
+    std::cout<<"Enter the value of n: ";
+    std::cin>>n;
+    int sum=0;
+    int i=1;
+    while(i<=n){
+        sum+=i;
+        i++; //i+=2 for sum of even numbers till n
+    }
+    std::cout<<"Sum is: "<<sum<<std::endl;
+    return 0;
+}
