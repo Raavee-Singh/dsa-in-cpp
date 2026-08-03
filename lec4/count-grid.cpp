a2fd75550b65d697616ebe1ad77c69b27fe1b57b
Raavee Singh <gdg1465@gmail.com>
2026-08-03 09:01:00 +0530

    day4 start

--- lec4/count-grid.cpp
+++ lec4/count-grid.cpp
@@ -3,13 +3,12 @@ int main(){
     int n;
     std::cout<<"Enter the value of n: ";
     std::cin>>n;
-    for(int i=0;i<n;i++){
-        int m=n;
-        for(int j=0;j<n;j++){
-            std::cout<<" "<<m<<" ";
-            m--;
+    int count=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            std::cout<<" "<<count<<" ";
+            count++; 
         }
         std::cout<<std::endl;
     }
-    return 0;
 }
