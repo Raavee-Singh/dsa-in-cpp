f93d74c9f9a1bcc75536fa0f278794b75b08a9fd
Raavee Singh <gdg1465@gmail.com>
2026-08-05 09:32:03 +0530

    starting lec10

---
+++ lec10/swap-alternate.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+void swapAlternate(int arr[],int size){
+    int i=0;
+    while(i+1<size){
+        std::swap(arr[i],arr[i+1]);
+        i+=2;
+    }
+}
+void printArray(int arr[],int size){
+    std::cout<<"Array is: ";
+    for(int i=0;i<size;i++){
+        std::cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int brr[6]={1,2,3,4,5,6};
+    swapAlternate(arr,5);
+    swapAlternate(brr,6);
+    printArray(arr,5);
+    std::cout<<std::endl;
+    printArray(brr,6);
+    return 0;
+}
---
+++ lec10/unique-element-array.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int uniqueArray(int arr[],int size){
+    int uniqueElement=0;
+    
+}
+int main(){
+    int arr[5]={1,2,1,3,2};
+
+}
