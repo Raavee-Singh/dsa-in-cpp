1aa70ecc55f54cc20a568b45b25e8846b8ef42e8
Raavee Singh <gdg1465@gmail.com>
2026-08-13 20:15:17 +0530

    completed continue and break

--- lec005/pre-post-in-de.cpp
+++ lec005/pre-post-in-de.cpp
@@ -7,5 +7,14 @@ int main(){
     std::cout<<"Post-increment: "<<a++<<std::endl;
     std::cout<<"Pre-decrement: "<<--a<<std::endl;
     std::cout<<"Post-decrement: "<<a--<<std::endl;
+    for(int i=0;i<10;i++){
+        if(i==4){
+            continue;
+        }
+        if(i==6){
+            break;
+        }
+        std::cout<<i<<std::endl;
+    }
     return 0;
 }
