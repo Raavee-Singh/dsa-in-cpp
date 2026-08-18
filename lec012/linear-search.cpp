001a4439dd1e84f9934c0e78480a4ea4eb47301a
Raavee Singh <gdg1465@gmail.com>
2026-08-18 20:51:24 +0530

    linear search

---
+++ lec012/linear-search.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+int linearSearch(int *arr,int size,int key){
+    for(int i=0;i<size;i++){
+        if(key==arr[i]){
+            return i;
+        }
+    }
+    return -1;
+}
+void acceptArray(int *arr,int size){
+    std::cout<<"Enter the elements of array: ";
+    for(int i=0;i<size;i++){
+        std::cin>>arr[i];
+    }
+}
+int main(){
+    int arr[10];
+    int size=sizeof(arr)/sizeof(int); 
+    int key;
+    acceptArray(arr,size);
+    std::cout<<"Enter the element to be searched: ";
+    std::cin>>key;
+    int result=linearSearch(arr,size,key);
+    if(result!=-1){
+        std::cout<<"Element found at "<<result<<std::endl;
+    }
+    else{
+        std::cout<<"Element not found"<<std::endl;
+    }
+    return 0;
+}
