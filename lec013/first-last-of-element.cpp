e78d34b02e2a5509396fd9e802bb4237c1b71e5d
Raavee Singh <gdg1465@gmail.com>
2026-09-01 07:29:12 +0530

    new changes

--- lec013/first-last-of-element.cpp
+++ lec013/first-last-of-element.cpp
@@ -49,12 +49,12 @@ int main(){
     int arr[6];
     acceptArray(arr,6);
     int key;
-    std::cout<<"Enter the key: ";
+    std::cout<<"Enter the element to be searched: ";
     std::cin>>key;
     std::pair<int,int> p;
     p.first=firOcc(arr,6,key);
     p.second=lastOcc(arr,6,key);
-    std::cout<<"The first and last position of "<<key<<"is: ";
+    std::cout<<"The first and last position of "<<key<<" is: ";
     std::cout<<p.first<<", "<<p.second<<std::endl;
     return 0;
 }
